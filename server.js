a067432540683581b5516dcabac8beffad2839d4
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2023-09-01 12:25:07 +0200

    mongoose intro

--- server.js
+++ server.js
@@ -1,24 +1,92 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.set("view engine", "hbs");
-app.set("views", __dirname + "/views");
+mongoose
+  .connect("mongodb://127.0.0.1:27017/examples")
+  .then((resp) => console.log("connected to", resp.connections[0].name))
+  .catch((err) => {
+    throw new Error(err);
+  });
 
-const greeting = "Hello from JS file";
+// mongoose.connection.on("connected", () => {
+//   console.log("connected with mongoDB");
+// });
+// const Cat = mongoose.model("Cat", { name: String });
+// const kitty = new Cat({ name: "cat1" });
+// console.log(kitty);
+// kitty
+//   .save()
+//   .then((resp) => console.log(resp))
+//   .catch((err) => {
+//     throw new Error(err);
+//   });
 
-app.get("/", (req, res) => {
-  res.render("index", { greeting });
-});
+// Cat.find()
+//   .then((allCats) => console.log(allCats))
+//   .catch((err) => {
+//     throw new Error(err);
+//   });
 
-let cities = ["Miami", "Madrid", "Barcelona"];
+// app.get("/", (request, response) => {
+//   Cat.find()
+//     .then((allCats) => {
+//       request.send(allCats);
+//     })
+//     .catch((err) => {
+//       throw new Error(err);
+//     });
+// });
 
-app.get("/cities", (req, res) => {
-  const newCity = req.query.newCity;
-  console.log(newCity);
-  if (newCity) {
-    cities.push(newCity);
-  }
-  res.render("cities", { cities });
+// function addNewCat(catName) {
+//   const kitty = new Cat({ name: catName });
+
+//   kitty
+//     .save()
+//     .then((newCat) => console.log(`A new cat is created: ${newCat}!`))
+//     .catch((err) => console.log(`Error while creating a new cat: ${err}`));
+// }
+
+// function showCats() {
+//   console.log("All the CATS!");
+//   Cat.find()
+//     .then((catsFromDB) => {
+//       // catsFromDB is an array of Cat instances
+//       catsFromDB.forEach((oneCat) => console.log(` --> cat: ${oneCat.name}`));
+//     })
+//     .catch((err) =>
+//       console.log(`Error occurred during getting cats from DB: ${err}`)
+//     );
+// }
+
+// function addTenCats() {
+//   for (let i = 0; i < 10; i++) {
+//     addNewCat(`Ironhacker ${i}`);
+//   }
+// }
+// // addTenCats();
+// // console.log("Hi There");
+// app.get("/", (req, res) => {
+//   console.log("Hi There");
+//   Cat.find().then((allCats) => res.send(allCats));
+// });
+
+const Student = mongoose.model("Student", { firstName: String });
+const City = mongoose.model("City", { name: String });
+
+const promise1 = Student.insertMany([
+  { firstName: "Alice" },
+  { firstName: "Bob" },
+]);
+
+const promise2 = City.insertMany([
+  { name: "Madrid" },
+  { name: "Barcelona" },
+  { name: "Paris" },
+]);
+
+Promise.all([promise1, promise2]).then((values) => {
+  console.log(values);
 });
 
 app.listen(3000, (err) => {
