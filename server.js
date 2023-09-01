11d638ff85624b18104d24d948a8b35b6297b7e4
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2023-09-01 10:08:17 +0200

    data cant be saved on the server without dbms

--- server.js
+++ server.js
@@ -5,8 +5,20 @@ app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
 
 const greeting = "Hello from JS file";
+
 app.get("/", (req, res) => {
   res.render("index", { greeting });
+});
+
+let cities = ["Miami", "Madrid", "Barcelona"];
+
+app.get("/cities", (req, res) => {
+  const newCity = req.query.newCity;
+  console.log(newCity);
+  if (newCity) {
+    cities.push(newCity);
+  }
+  res.render("cities", { cities });
 });
 
 app.listen(3000, (err) => {
